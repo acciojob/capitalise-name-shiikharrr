deeee5c89ade5cb6d18e03b53199ca21e9d54a91
shiikharrr <69209995+shiikharrr@users.noreply.github.com>
2026-01-03 21:03:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const input = document.getElementById('fname');
+if (input){
+	input.addEventListener('blur', function(){
+		this.value = this.value.toUpperCase();
+	});
+}
